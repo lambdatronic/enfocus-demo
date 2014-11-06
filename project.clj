471778a61379dda1a9dfc39da3fba1ed34d0e02a
Gary Johnson <gwjohnso@uvm.edu>
2014-11-06 16:11:48 -0500

    Upgrade enfocus to 2.1.1.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2356"]
-                 [enfocus "2.1.0"]]
+                 [enfocus "2.1.1"]]
   :cljsbuild {:builds {:main {:source-paths ["src/cljs"]
                               :compiler {:output-to     "resources/js/script.js"
                                          :optimizations :advanced}}}})
