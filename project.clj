bb1ec17bcd14d8fdf4a2ddf196cf2ceee9407711
Gary Johnson <gwjohnso@uvm.edu>
2014-11-06 15:18:23 -0500

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject enfocus-demo "0.1.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2356"]
+                 [enfocus "2.1.0"]]
+  :cljsbuild {:builds {:main {:source-paths ["src/cljs"]
+                              :compiler {:output-to     "resources/js/script.js"
+                                         :optimizations :advanced}}}})
---
+++ src/cljs/enfocus_demo/core.cljs
@@ -0,0 +1,19 @@
+(ns enfocus-demo.core
+  (:require [enfocus.core :as ef]
+            [enfocus.effects :as eff]
+            [enfocus.events :as ev])
+  (:require-macros [enfocus.macros :as em]))
+
+(em/defaction change [msg]
+  "#button1" (ef/content msg))
+
+(em/defaction resize-div [width]
+  "#rz-demo" (eff/chain
+              (eff/resize width :curheight 500)
+              (eff/resize 5 :curheight 500)))
+
+(em/defaction setup []
+  "#button1" (ev/listen :click #(change "I have been clicked"))
+  "#button2" (ev/listen :click #(resize-div 200)))
+
+(set! (.-onload js/window) setup)
